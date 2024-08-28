bd525bf9a00dabc87b5998f753380eee4111463a
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-08-28 23:46:16 +0530

    upd

--- 01-Python Crash Course1.py
+++ 01-Python Crash Course1.py
@@ -9,10 +9,8 @@
 # 
 # Please note, this is not meant to be a comprehensive overview of Python or programming in general, if you have no programming experience, you should probably take my other course: [Complete Python Bootcamp](https://www.udemy.com/complete-python-bootcamp/?couponCode=PY20) instead.
 # 
-# **This notebook is just a code reference for the videos, no written explanations here**
-# 
+# **This notebook is just a code reference for the videos, no written explanations here** 
 # This notebook will just go through the basic topics in order:
-# 
 # * Data types
 #     * Numbers
 #     * Strings
