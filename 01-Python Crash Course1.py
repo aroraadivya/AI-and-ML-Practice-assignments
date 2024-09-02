712b63a1cdf2f41d7881b6383b566191778af637
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-02 19:29:10 +0530

    upd

--- 01-Python Crash Course1.py
+++ 01-Python Crash Course1.py
@@ -3,7 +3,6 @@
 # <a href='http://www.pieriandata.com'> <img src='../Pierian_Data_Logo.png' /></a>
 # # Python Crash Course
 # Please note, this is not meant to be a comprehensive overview of Python or programming in general, if you have no programming experience, you should probably take my other course: [Complete Python Bootcamp](https://www.udemy.com/complete-python-bootcamp/?couponCode=PY20) instead.
-# 
 # **This notebook is just a code reference for the videos, no written explanations here** 
 # This notebook will just go through the basic topics in order:
 # * Data types
